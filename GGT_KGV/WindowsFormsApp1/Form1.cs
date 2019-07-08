c97e5b6fbd22d7e495566ff83c56c9f9d901aa1f
NoobyNoob1983 <49959375+NoobyNoob1983@users.noreply.github.com>
2019-07-08 08:17:05 +0200

    Add files via upload

---
+++ GGT_KGV/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.textBox4.Text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum.sanctus sea sed takimata ut vero voluptua.est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisisAt vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, At accusam aliquyam diam diam dolore dolores duo eirmod eos erat, et nonumy sed tempor et et invidunt justo labore Stet clita ea et gubergren, kasd magna no rebum.sanctus sea sed takimata ut vero voluptua.est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat.Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit";
+
+        }
+
+        // Textbox 1 ChangeText
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int zahl = 0;
+            int zahl2 = 0;
+            if (int.TryParse(textBox1.Text, out zahl) && int.TryParse(textBox2.Text, out zahl2))
+            {
+                button1.Text = "Gültig";
+                button2.Text = "Gültig";
+                button1.BackColor = Color.Green;
+                button2.BackColor = Color.Green;
+            }
+            else
+            {
+                button1.Text = "Ungültig";
+                button1.BackColor = Color.Red;
+                button2.BackColor = Color.Red;
+                button2.Text = "Ungültig";
+            }
+        }
+
+        /// <summary>
+        /// Button Klick GGT
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int rechnung = 0;
+          
+            if (button1.Text == "Gültig" && button2.Text == "Gültig")
+            {
+                int box1 = int.Parse(textBox1.Text);
+                int box2 = int.Parse(textBox2.Text);
+                rechnung = GGTBerechnen(box1,box2);
+
+                label1.BackColor = Color.Green;
+                label1.Left = 100;
+                label1.Text = "Der GGT von "+ box1.ToString() + " und " + box2.ToString() + " lautet "+  rechnung.ToString();
+               
+            }
+            else
+            {
+                label1.Left = 100;
+                label1.BackColor = Color.Red;
+                label1.Text = "ich kann nur mit Zahlen rechnen";
+            }
+           
+        }
+
+        //ButtonKlick KGV
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int rechnung = 0;
+            if (button1.Text == "Gültig" && button2.Text == "Gültig")
+            {
+                int box1 = int.Parse(textBox1.Text);
+                int box2 = int.Parse(textBox2.Text);
+                rechnung = KGVBerechnen(box1, box2);
+
+                label1.BackColor = Color.Green;
+                label1.Left = 100;
+                label1.Text = "Der KGV von " + box1.ToString() + " und " + box2.ToString() + " lautet " + rechnung.ToString();
+
+            }
+            else
+            {
+                label1.Left = 100;
+                label1.BackColor = Color.Red;
+                label1.Text = "ich kann nur mit Zahlen rechnen";
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string text = textBox3.Text;
+            string rev = "";
+
+
+            if (!string.IsNullOrEmpty(textBox3.Text))
+            {
+
+
+                for (int i = textBox3.Text.Length; i > 0; i--)
+                {
+                    rev = rev += textBox3.Text[i - 1];
+                }
+
+                if (text == rev)
+                {
+                    label2.BackColor = Color.Green;
+                    label2.Text = rev + " ist ein Palindrom ";
+                }
+                else
+                {
+                    label2.BackColor = Color.Red;
+                    label2.Text = rev + " Ist Kein Palindrom";
+                }
+            }
+        }
+
+        // 
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            if (textBox5.Text.Length == 1)
+            {
+                for (int i = 0; i < textBox4.Text.Length; i++)
+                {
+                    if (textBox5.Text.ToLower() == textBox4.Text[i].ToString().ToLower())
+                    {
+                        counter++;
+                    }
+                }
+                label3.Text = "Die Häufigkeit von " + textBox5.Text + " ist " + counter;
+            }
+            else
+            {
+                label3.Text = "Wie wäre es mit nur einem Zeichen??";
+            }
+        }
+
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox3.Text))
+            {
+                button3.BackColor = Color.Red;
+            }
+            else
+            {
+                button3.BackColor = Color.Green;
+            }
+
+        }
+
+
+
+        /// <summary>
+        /// Diese Funktion berechnet den GrösstenGemeinsamenTeiler
+        /// </summary>
+        /// <param name="zahl1"></param>
+        /// <param name="zahl2"></param>
+        /// <returns></returns>
+        static int GGTBerechnen(int zahl1, int zahl2)
+        {
+
+            if (zahl2 == 0)
+            {
+                return zahl1;
+            }
+            else
+            {
+                return GGTBerechnen(zahl2, zahl1 % zahl2);
+            }
+        }
+        /// <summary>
+        /// Diese Funktion berechnet das kleinste gemeinsame Vielfache
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        static int KGVBerechnen(int a, int b)
+        {
+            int big;
+            if (a == b)
+            { return a; }
+            else if (a > b)
+            {
+                big = a;
+            }
+            else
+            {
+                big = b;
+            }
+            for (int i = big; i < a * b; i++)
+            {
+                if (i % a == 0 && i % b == 0)
+                { return i; }
+            }
+            return a * b;
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    
+}
