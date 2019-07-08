dbd84b3bf8b625003aea520781308b8b9cdf6a1a
NoobyNoob1983 <49959375+NoobyNoob1983@users.noreply.github.com>
2019-07-08 08:08:49 +0200

    Add files via upload

---
+++ TicTacToe Bot Version/TicTacToe/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AccessibleRole = System.Windows.Forms.AccessibleRole.Caret;
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 94F));
+            this.tableLayoutPanel1.Controls.Add(this.textBox7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox9, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(368, 189);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 52F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 48F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(296, 255);
+            this.tableLayoutPanel1.TabIndex = 1;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.TableLayoutPanel1_Paint);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox7.Location = new System.Drawing.Point(3, 178);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(95, 74);
+            this.textBox7.TabIndex = 6;
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox7.Click += new System.EventHandler(this.textBox1_click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(205, 94);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(88, 78);
+            this.textBox6.TabIndex = 5;
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox6.Click += new System.EventHandler(this.textBox1_click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(3, 94);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(95, 78);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox4.Click += new System.EventHandler(this.textBox1_click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(205, 3);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(88, 85);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.Click += new System.EventHandler(this.textBox1_click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(104, 3);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(95, 85);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.Click += new System.EventHandler(this.textBox1_click);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.textBox8.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox8.Location = new System.Drawing.Point(104, 178);
+            this.textBox8.Multiline = true;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(95, 74);
+            this.textBox8.TabIndex = 7;
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox8.Click += new System.EventHandler(this.textBox1_click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(104, 94);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(95, 78);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox5.Click += new System.EventHandler(this.textBox1_click);
+            // 
+            // textBox9
+            // 
+            this.textBox9.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.textBox9.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox9.Location = new System.Drawing.Point(205, 178);
+            this.textBox9.Multiline = true;
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(88, 74);
+            this.textBox9.TabIndex = 8;
+            this.textBox9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox9.Click += new System.EventHandler(this.textBox1_click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.HideSelection = false;
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(95, 85);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(234, 492);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(568, 23);
+            this.progressBar1.TabIndex = 2;
+            this.progressBar1.EnabledChanged += new System.EventHandler(this.textBox1_click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.label1.Location = new System.Drawing.Point(362, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 36);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(976, 725);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "SuperDuperTicTacToe";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TicTacToe Bot Version/TicTacToe/Form1.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    
+    public partial class Form1 : Form
+    {
+
+        static Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Spieler X ist dran";
+            
+        }
+        
+
+        string lastKey = "";
+        string key = "";
+        string text = "";
+        bool win = false;
+        private void TableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+
+        private void textBox1_click(object sender, EventArgs e)
+        {
+            TextBox textBoxen = (TextBox)sender;
+
+            label1.Text = "Spieler " + key + " ist dran";
+
+            // RealPLayer
+            if (lastKey == "" || lastKey == "O")
+            {
+                if (textBoxen.Text == "")
+                {
+                    textBoxen.Text = "X";
+                    lastKey = "X";
+                    key = "O";
+                }
+
+            }
+            textBoxen.Enabled = false;
+            progressBar1.Increment(12);
+
+
+            win = false;
+
+            CheckWin(ref lastKey);
+
+            textBox1_TextChanged(sender, e);
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            System.Threading.Thread.Sleep(20);
+
+
+            TextBox[] boxen = new TextBox[9];
+            boxen[0] = textBox1;
+            boxen[1] = textBox2;
+            boxen[2] = textBox3;
+            boxen[3] = textBox4;
+            boxen[4] = textBox5;
+            boxen[5] = textBox6;
+            boxen[6] = textBox7;
+            boxen[7] = textBox8;
+            boxen[8] = textBox9;
+
+            int zufall = random.Next(0, 9);
+
+
+            if (lastKey == "X")
+            {
+                CheckWin(ref lastKey);
+
+                
+                if (boxen[8].Text == "X" && boxen[4].Text == "X" || boxen[8].Text == "O" && boxen[4].Text == "O" && boxen[0].Enabled == true)
+                {
+                    zufall = 0;
+                }
+                else if (boxen[1].Text == "X" && boxen[2].Text == "X" || boxen[1].Text == "O" && boxen[2].Text == "O"  && boxen[0].Enabled == true)
+                {
+                    zufall = 0;
+                }
+
+
+                if (boxen[0].Text == "X" && boxen[2].Text == "X" || boxen[0].Text == "O" && boxen[2].Text == "O"  && boxen[1].Enabled == true)
+                {
+                    zufall = 1;
+                }
+                else if(boxen[7].Text == "X" && boxen[4].Text == "X" || boxen[7].Text == "O" && boxen[4].Text == "O" && boxen[1].Enabled == true)
+                {
+                    zufall = 1;
+                }
+
+
+                if (boxen[0].Text == "X" && boxen[1].Text == "X" || boxen[0].Text == "O" && boxen[1].Text == "O" && boxen[2].Enabled == true)
+                {
+                    zufall = 2;
+                }
+                else if(boxen[8].Text == "X" && boxen[5].Text == "X" || boxen[8].Text == "O" && boxen[5].Text == "O" && boxen[2].Enabled == true)
+                {
+                    zufall = 2;
+                }
+
+
+                if (boxen[0].Text == "X" && boxen[6].Text == "X" || boxen[0].Text == "O" && boxen[6].Text == "O" && boxen[3].Enabled == true)
+                {
+                    zufall = 3;
+                }
+                else if(boxen[4].Text == "X" && boxen[5].Text == "X" || boxen[4].Text == "O" && boxen[5].Text == "O" && boxen[3].Enabled == true)
+                {
+                    zufall = 3;
+                }
+
+
+                if (boxen[2].Text == "X" && boxen[6].Text == "X" || boxen[2].Text == "O" && boxen[6].Text == "O" && boxen[4].Enabled == true)
+                {
+                    zufall = 4;
+                }
+                else if(boxen[0].Text == "X" && boxen[8].Text == "X" || boxen[0].Text == "O" && boxen[8].Text == "O" && boxen[4].Enabled == true)
+                {
+                    zufall = 4;
+                }
+                else if(boxen[3].Text == "X" && boxen[5].Text == "X" || boxen[3].Text == "O" && boxen[5].Text == "O" && boxen[4].Enabled == true)
+                {
+                    zufall = 4;
+                }
+                else if(boxen[1].Text == "X" && boxen[7].Text == "X" || boxen[1].Text == "O" && boxen[7].Text == "O" && boxen[4].Enabled == true)
+                {
+                    zufall = 4;
+                }
+
+
+                if(boxen[3].Text == "X" && boxen[4].Text == "X" || boxen[3].Text == "O" && boxen[4].Text == "O" && boxen[5].Enabled == true)
+                {
+                    zufall = 5;
+                }
+                else if(boxen[2].Text == "X" && boxen[3].Text == "X" || boxen[2].Text == "O" && boxen[3].Text == "O" && boxen[5].Enabled == true)
+                {
+                    zufall = 5;
+                }
+
+                // geändert 0 : 3 in 0 : 4
+                if (boxen[0].Text == "X" && boxen[3].Text == "X" || boxen[0].Text == "O" && boxen[3].Text == "O" && boxen[6].Enabled == true)
+                {
+                    zufall = 6;
+                }
+                else if(boxen[7].Text == "X" && boxen[8].Text == "X" || boxen[7].Text == "O" && boxen[8].Text == "O" && boxen[6].Enabled == true)
+                {
+                    zufall = 6;
+                }
+
+
+                if(boxen[6].Text == "X" && boxen[8].Text == "X" || boxen[6].Text == "O" && boxen[8].Text == "O" && boxen[7].Enabled == true)
+                {
+                    zufall = 7;
+                }
+                else if(boxen[1].Text == "X" && boxen[4].Text == "X" || boxen[1].Text == "O" && boxen[4].Text == "O" && boxen[7].Enabled == true)
+                {
+                    zufall = 7;
+                }
+
+                if (boxen[2].Text == "X" && boxen[5].Text == "X"  || boxen[2].Text == "O" && boxen[5].Text == "O" && boxen[8].Enabled == true)
+                {
+                    zufall = 8;
+                }
+                else if(boxen[6].Text == "X" && boxen[7].Text == "X" || boxen[6].Text == "O" && boxen[7].Text == "O" && boxen[8].Enabled == true )
+                {
+                    zufall = 8;
+                }
+                else if(boxen[6].Text == "X" && boxen[7].Text == "X" || boxen[6].Text == "O" && boxen[7].Text == "O"  && boxen[8].Enabled == true)
+                {
+                    zufall = 8;
+                }
+                else if(boxen[0].Text == "X" && boxen[4].Text == "X" || boxen[0].Text == "O" && boxen[4].Text == "O"  && boxen[8].Enabled == true)
+                {
+                    zufall = 8;
+                }
+
+
+                while (boxen[zufall].Enabled == false)
+                {
+                    zufall = random.Next(0, 9);
+                }
+
+
+                System.Threading.Thread.Sleep(250);
+
+                boxen[zufall].Text = "O";
+                boxen[zufall].Enabled = false;
+
+                
+                lastKey = "O";
+                key = "X";
+
+               
+
+                textBox1_click(sender, e);
+            }
+        }
+
+
+        public void CheckWin(ref string lastKey)
+        {
+            text = " " + lastKey + " hat gewonnen";
+
+            if (textBox1.Text == "X" && textBox2.Text == "X" && textBox3.Text == "X" || textBox1.Text == "O" && textBox2.Text == "O" && textBox3.Text == "O")
+            {
+                var result = MessageBox.Show(text, "Text", MessageBoxButtons.RetryCancel);
+
+                if (result == DialogResult.Cancel) { System.Environment.Exit(-1); } else { win = false; lastKey = ""; key = ""; }
+                win = true;
+            }
+            else if (textBox4.Text == "X" && textBox5.Text == "X" && textBox6.Text == "X" || textBox4.Text == "O" && textBox5.Text == "O" && textBox6.Text == "O")
+            {
+                var result = MessageBox.Show(text, "Text", MessageBoxButtons.RetryCancel);
+                if (result == DialogResult.Cancel) { System.Environment.Exit(-1); }
+                win = true;
+
+            }
+            else if (textBox7.Text == "X" && textBox8.Text == "X" && textBox9.Text == "X" || textBox7.Text == "O" && textBox8.Text == "O" && textBox9.Text == "O")
+            {
+                var result = MessageBox.Show(text, "Text", MessageBoxButtons.RetryCancel);
+                if (result == DialogResult.Cancel) { System.Environment.Exit(-1); }
+                win = true;
+            }
+            else if (textBox1.Text == "X" && textBox4.Text == "X" && textBox7.Text == "X" || textBox1.Text == "O" && textBox4.Text == "O" && textBox7.Text == "O")
+            {
+                var result = MessageBox.Show(text, "Text", MessageBoxButtons.RetryCancel);
+                if (result == DialogResult.Cancel) { System.Environment.Exit(-1); }
+                win = true;
+            }
+            else if (textBox2.Text == "X" && textBox5.Text == "X" && textBox8.Text == "X" || textBox2.Text == "O" && textBox5.Text == "O" && textBox8.Text == "O")
+            {
+                var result = MessageBox.Show(text, "Text", MessageBoxButtons.RetryCancel);
+                if (result == DialogResult.Cancel) { System.Environment.Exit(-1); }
+                win = true;
+            }
+            else if (textBox3.Text == "X" && textBox6.Text == "X" && textBox9.Text == "X" || textBox3.Text == "O" && textBox6.Text == "O" && textBox9.Text == "O")
+            {
+                var result = MessageBox.Show(text, "Text", MessageBoxButtons.RetryCancel);
+                if (result == DialogResult.Cancel) { System.Environment.Exit(-1); }
+                win = true;
+            }
+            else if (textBox1.Text == "X" && textBox5.Text == "X" && textBox9.Text == "X" || textBox1.Text == "O" && textBox5.Text == "O" && textBox9.Text == "O")
+            {
+                var result = MessageBox.Show(text, "Text", MessageBoxButtons.RetryCancel);
+                if (result == DialogResult.Cancel) { System.Environment.Exit(-1); }
+                win = true;
+            }
+            else if (textBox3.Text == "X" && textBox5.Text == "X" && textBox7.Text == "X" || textBox3.Text == "O" && textBox5.Text == "O" && textBox7.Text == "O")
+            {
+                var result = MessageBox.Show(text, "Gewonnen", MessageBoxButtons.RetryCancel);
+                if (result == DialogResult.Cancel) { System.Environment.Exit(-1); }
+                win = true;
+            }
+            else if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "" && textBox4.Text != "" && textBox5.Text != "" && textBox6.Text != "" && textBox7.Text != "" && textBox8.Text != "" && textBox9.Text != "")
+            {
+                var result = MessageBox.Show("Keiner hat Gewonnen", "Text", MessageBoxButtons.RetryCancel);
+                if (result == DialogResult.Cancel) { System.Environment.Exit(-1); }
+                win = true;
+            }
+
+
+
+            if (win == true)
+            {
+                textBox1.Text = "";
+                textBox1.Enabled = true;
+                textBox2.Text = "";
+                textBox2.Enabled = true;
+                textBox3.Text = "";
+                textBox3.Enabled = true;
+                textBox4.Text = "";
+                textBox4.Enabled = true;
+                textBox5.Text = "";
+                textBox5.Enabled = true;
+                textBox6.Text = "";
+                textBox6.Enabled = true;
+                textBox7.Text = "";
+                textBox7.Enabled = true;
+                textBox8.Text = "";
+                textBox8.Enabled = true;
+                textBox9.Text = "";
+                textBox9.Enabled = true;
+
+                lastKey = "";
+
+            }
+        }
+
+
+    }
+}
